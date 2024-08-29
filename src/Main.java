677c5dc1680f824d2dc208240d3030c8bd1884ad
Mark Barnes <barnemar002@warren.k12.in.us>
2024-08-29 12:51:27 -0400

    initial

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+    }
+
+    private static boolean isCatPlaying(boolean isSummer, int temperature) {
+        if (isSummer) {
+            if (temperature >= 25 && temperature <= 45) {
+                return true;
+            }
+        } else {
+            if (temperature >= 25 && temperature <= 35) {
+                return true;
+            }
+        }
+        return isSummer;
+    }
+
+
+}
